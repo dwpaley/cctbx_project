powder_util: remove angle_histogram feature and fix n_max off-by-one
Remove the superseded angle_histogram feature entirely: the angle() and
create_pairwise_plots() top-level functions, the per-experiment and
per-reflection angle accumulation code, and the post-loop histogram plot.
Also remove import itertools (only used by that code) and the duplicate
import numpy as np.

Fix n_max off-by-one: change i>n_max to i>=n_max so that n_max=10
processes exactly 10 experiments (indices 0-9).

xfel/small_cell/powder_util.py
@@ -3,72 +3,12 @@ from dxtbx.model.experiment_list import DetectorComparison
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tick
-import numpy as np
 import copy
 from dials.array_family import flex
 from cctbx import uctbx
 from scitbx.math import five_number_summary
 from cctbx.crystal import symmetry
 import cctbx.miller
-import itertools
-
-def angle(v1, v2):
-    """
-    Compute the angle between two cartesian vectors.
-
-    Parameters:
-    v1 (numpy.ndarray): The first vector.
-    v2 (numpy.ndarray): The second vector.
-
-    Returns:
-    float: The angle between the vectors in degrees.
-    """
-    dot_product = np.dot(v1, v2)
-    magnitude_v1 = np.linalg.norm(v1)
-    magnitude_v2 = np.linalg.norm(v2)
-    cos_theta = dot_product / (magnitude_v1 * magnitude_v2)
-    return np.degrees(np.arccos(cos_theta))
-
-def create_pairwise_plots(points, labels):
-    # Create a figure with 3 subplots
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
-
-    # Define custom color map
-    color_map = {
-        -1: 'lightgray',  # outliers in light gray
-        0: 'red',
-        1: 'blue',
-        2: 'orange',
-        3: 'green'
-    }
-
-    # Convert labels to colors
-    colors = [color_map[label] if label in color_map else 'gray' for label in labels]
-
-    # Define scatter plot properties
-    scatter_kwargs = {'c': labels, 'cmap': 'viridis', 's': 1, 'alpha': 0.6}
-    scatter_kwargs = {'c': colors,  's': 1, 'alpha': 0.6}
-
-    # Plot ab
-    ax1.scatter(points[:, 0], points[:, 1], **scatter_kwargs)
-    ax1.set_xlabel('a')
-    ax1.set_ylabel('b')
-    ax1.set_title('a vs b')
-
-    # Plot ac
-    ax2.scatter(points[:, 0], points[:, 2], **scatter_kwargs)
-    ax2.set_xlabel('a')
-    ax2.set_ylabel('c')
-    ax2.set_title('a vs c')
-
-    # Plot bc
-    ax3.scatter(points[:, 1], points[:, 2], **scatter_kwargs)
-    ax3.set_xlabel('b')
-    ax3.set_ylabel('c')
-    ax3.set_title('b vs c')
-
-    plt.tight_layout()
-    plt.show()
 
 class Spotfinder_radial_average:
 
@@ -160,15 +100,6 @@ class Spotfinder_radial_average:
         assert compare_detector(ref_detector, expt.detector)
         expt.detector = detector
 
-    if params.angle_histogram.enable:
-      if 's1' not in refls.keys():
-        refls.centroid_px_to_mm(expts)
-        refls.map_centroids_to_reciprocal_space(expts)
-      angles_12 = []
-      angles_13 = []
-      angles_23 = []
-      detplot_counter = 0
-
     for i, expt in enumerate(expts):
       self.current_panelsums = [
           np.zeros(params.n_bins) for _ in range(self.n_panels)
@@ -184,7 +115,7 @@ class Spotfinder_radial_average:
       else:
         self.use_current_expt = True
       if i % 1000 == 0: print("experiment ", i)
-      if self.params.n_max is not None and i>self.params.n_max:
+      if self.params.n_max is not None and i >= self.params.n_max:
         break
       s0 = expt.beam.get_s0()
       sel = refls['id'] == i
@@ -193,12 +124,6 @@ class Spotfinder_radial_average:
       intensities = refls_sel['intensity.sum.value']
       panels = refls_sel['panel']
 
-      if params.angle_histogram.enable: 
-        r1max, r1min = params.angle_histogram.range1
-        r2max, r2min = params.angle_histogram.range2
-        r3max, r3min = params.angle_histogram.range3
-
-        i_r1, i_r2 ,i_r3 = [],[],[]
       for i_refl in range(len(refls_sel)):
         self.expt_count += 1
         i_panel = panels[i_refl]
@@ -211,61 +136,6 @@ class Spotfinder_radial_average:
           else:
             value = 1
           res = self._process_pixel(i_panel, s0, panel, xy, value)
-          if params.angle_histogram.enable and r1max > res > r1min:
-            i_r1.append(i_refl)
-          if params.angle_histogram.enable and r2max > res > r2min:
-            i_r2.append(i_refl)
-          if params.angle_histogram.enable and r3max > res > r3min:
-            i_r3.append(i_refl)
-
-      if params.angle_histogram.enable and i_r1 and i_r2 and i_r3:
-        i_all = i_r1 + i_r2 + i_r3
-        a12, a13, a23 = [],[],[]
-        subsel_mask = flex.bool([n in i_all for n in range(len(refls_sel))])
-        subsel_mask_inv = flex.bool([not x for x in subsel_mask])
-
-#        subsel = refls_sel.select(subsel_mask)
-#        subsel_inv = refls_sel.select(subsel_mask_inv)
-#        xyz1 = np.array(subsel['xyzobs.mm.value'])
-#        xyz2 = np.array(subsel_inv['xyzobs.mm.value'])
-#        plt.scatter(xyz1[:,0], xyz1[:,1], c='red', s=2)
-#        plt.scatter(xyz2[:,0], xyz2[:,1], c='blue', s=2)
-#        bc = expt.detector[0].get_beam_centre(expt.beam.get_s0())
-#        plt.scatter(*bc, c='k', s=5)
-#        plt.xlim((100,240))
-#        plt.ylim((100,240))
-          
-        s0 = np.array(expt.beam.get_s0())
-        for i1, i2, i3 in itertools.product(i_r1, i_r2, i_r3):
-          v1 = np.array(refls_sel[i1]['s1']) - s0
-          v2 = np.array(refls_sel[i2]['s1']) - s0
-          v3 = np.array(refls_sel[i3]['s1']) - s0
-          a12.append(angle(v1, v2))
-          a13.append(angle(v1, v3))
-          a23.append(angle(v2, v3))
-#        print('\n----------------------')
-#        print('Pairwise angles:')
-#        headers = '1,2: 59, 121', '1,3: 30, 150', '2,3: 25, 155'
-#        for header, vals in zip(headers, (a12, a13, a23)):
-#          print()
-#          print(header)
-#          for v in vals: print(round(v, 2))
-#        plt.show()
-        angles_12.extend(a12)
-        angles_13.extend(a13)
-        angles_23.extend(a23)
-#        for i1, i2 in itertools.product(i_r1, i_r2):
-#          v1 = np.array(refls_sel[i1]['s1']) - s0
-#          v2 = np.array(refls_sel[i2]['s1']) - s0
-#          angles_12.append(angle(v1, v2))
-#        for i1, i2 in itertools.product(i_r1, i_r3):
-#          v1 = np.array(refls_sel[i1]['s1']) - s0
-#          v2 = np.array(refls_sel[i2]['s1']) - s0
-#          angles_13.append(angle(v1, v2))
-#        for i1, i2 in itertools.product(i_r2, i_r3):
-#          v1 = np.array(refls_sel[i1]['s1']) - s0
-#          v2 = np.array(refls_sel[i2]['s1']) - s0
-#          angles_23.append(angle(v1, v2))
 
       for i in range(len(self.panelsums)):
         self.panelsums[i] = self.panelsums[i] + self.current_panelsums[i]
@@ -285,22 +155,6 @@ class Spotfinder_radial_average:
         for i in range(len(self.panelsums)):
           self.antifiltered_panelsums[i] = \
               self.antifiltered_panelsums[i] + self.current_panelsums[i]
-    if params.angle_histogram.enable:
-#      from sklearn.cluster import DBSCAN
-#      data=np.vstack((angles_12, angles_13, angles_23)).transpose()
-#      dbscan = DBSCAN(eps=6, min_samples=15)
-#      labels = dbscan.fit_predict(data)
-#      create_pairwise_plots(data, labels)
-#      fig, (ax1, ax2, ax3) = plt.subplots(1,3)
-#      ax1.scatter(angles_12, angles_13, s=.5)
-#      ax2.scatter(angles_13, angles_23, s=.5)
-#      ax3.scatter(angles_12, angles_23, s=.5)
-      fig, (ax1, ax2, ax3) = plt.subplots(3,1)
-      ax1.hist(angles_12, bins=180)
-      ax2.hist(angles_13, bins=180)
-      ax3.hist(angles_23, bins=180)
-
-      plt.show()
     self.dvals = np.array(self.dvals)
 
 